44eb3ca950d0f01a225aa696299ee758f6f6521c
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-09 17:45:59 +0530

    frontend boilerplate removed

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Open_Sans} from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const openSans = Open_Sans({subsets: ['latin']})
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <div className={openSans.className}>
+            <main>
+                <Component {...pageProps} />
+            </main>
+        </div>
+
+
+    )
 }

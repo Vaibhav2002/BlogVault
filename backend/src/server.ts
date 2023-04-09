ee548d96dade0a49bdae027dc3eb7dd73dae0fe9
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-09 18:43:20 +0530

    MongoDb Connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,13 @@
 import app from "./app";
+import cleanEnv from "./utils/CleanEnv"
+import * as mongoose from "mongoose";
 
-const port = 3000
+const port = cleanEnv.PORT
+const mongoConnectionString = cleanEnv.MONGO_CONNECTION_STRING
 
-app.listen(port, (req, res) => res.send(`Server is running on port ${port}`))
+mongoose.connect(mongoConnectionString)
+    .then(() => {
+        console.log("Connected to MongoDB")
+        app.listen(port, () => console.log(`Server running on port ${port}`))
+    })
+    .catch(console.error)

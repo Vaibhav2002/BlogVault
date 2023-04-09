0dfbc49620123c89914be6ef71a95a3e28501658
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-09 17:59:54 +0530

    MUI SRR Setup done

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Open_Sans} from "next/font/google";
+import {CacheProvider, EmotionCache} from "@emotion/react";
+import createEmotionCache from "@/utils/EmotionCache";
 
 const openSans = Open_Sans({subsets: ['latin']})
+const clientSideEmotionCache = createEmotionCache()
 
-export default function App({Component, pageProps}: AppProps) {
+interface AppComponentProps extends AppProps {
+    emotionCache: EmotionCache
+}
+
+export default function App({Component, pageProps, emotionCache = clientSideEmotionCache}: AppComponentProps) {
     return (
-        <div className={openSans.className}>
-            <main>
-                <Component {...pageProps} />
-            </main>
-        </div>
-
-
+        <CacheProvider value={emotionCache}>
+            <div className={openSans.className}>
+                <main>
+                    <Component {...pageProps} />
+                </main>
+            </div>
+        </CacheProvider>
     )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,9 @@
+import {Typography} from "@mui/material";
+
 export default function Home() {
   return (
     <>
+      <Typography variant="h4">Hello NextJs App</Typography>
     </>
   )
 }

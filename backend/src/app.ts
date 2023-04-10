d6bef82ef610ff3373dfbed4b203e7c47a6b9b29
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-10 20:46:08 +0530

    Added CORS to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,14 @@ import express from 'express';
 import {errorMiddleware, notFoundMiddleware} from "./middlewares/ErrorMiddlewares";
 import blogRoutes from "./routes/BlogRoutes";
 import TagRoutes from "./routes/TagRoutes";
+import cors from "cors";
+import env from "./utils/CleanEnv";
 
 const app = express()
+
+app.use(cors({
+    origin: env.WEBSITE_URL
+}))
 
 app.use(express.json())
 

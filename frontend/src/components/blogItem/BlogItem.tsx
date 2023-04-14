1d576cb883f9bb3c05ff72cfc9b3d7e77a4bdb0d
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-14 20:36:41 +0530

    Basic Blog Item design created

---
+++ frontend/src/components/blogItem/BlogItem.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Blog from "@/data/models/Blog";
+import {Avatar, Box, Typography} from "@mui/material";
+import styles from "./BlogItem.module.css";
+import MultilineText from "@/components/styled/MultilineText";
+import {FaArrowRight} from "react-icons/fa";
+import PrimaryButton from "@/components/styled/PrimaryButton";
+import {formatDate} from "@/utils/Helpers";
+import {CiBookmark} from "react-icons/ci";
+
+interface BlogItemProps {
+    blog: Blog
+    className?: string
+}
+
+const BlogItem = ({blog: {title, description, createdAt}, className}: BlogItemProps) => {
+
+    return (
+        <Box className={`${styles.blogCard} ${className}`}>
+            {/*Will be replaced by image*/}
+            <Box className={styles.blogImage}></Box>
+
+            <Box className={styles.blogContent}>
+                <Box>
+                    <MultilineText variant="h6" maxLines={1} marginBottom="4px">{title}</MultilineText>
+                    <MultilineText variant="caption" color="text.secondary" maxLines={3}>{description}</MultilineText>
+                </Box>
+                <Box className={styles.userInfoSection}>
+                    <Box className={styles.userInfo}>
+                        <Avatar variant="circular" className={styles.userAvatar}/>
+                        <Typography variant="body2" color="text.secondary">User</Typography>
+                    </Box>
+                    <Typography variant="body2" color="text.secondary">{formatDate(createdAt)}</Typography>
+                </Box>
+                <Box className={styles.actionSection}>
+                    <PrimaryButton
+                        size="small"
+                        startIcon={<CiBookmark/>}
+                        variant="text"
+                        sx={{paddingLeft: 0, color: "text.secondary"}}
+                    >
+                        Save for later
+                    </PrimaryButton>
+                    <PrimaryButton
+                        size="small"
+                        endIcon={<FaArrowRight size="16px"/>}
+                        variant="text"
+                        sx={{color: "text.secondary"}}
+                    >
+                        Read more
+                    </PrimaryButton>
+                </Box>
+            </Box>
+
+        </Box>
+    )
+}
+
+export default BlogItem;
+
+
+

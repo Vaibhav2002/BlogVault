d609a8ccf8729bca7265336fc4685179939bc3c0
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-04-20 13:02:59 +0530

    Modal opening animation added

--- frontend/src/components/modals/PrimaryModal.tsx
+++ frontend/src/components/modals/PrimaryModal.tsx
@@ -1,6 +1,7 @@
 import React, {ReactElement} from 'react';
 import styles from "@/styles/Modal.module.css"
 import {Box, Modal} from "@mui/material";
+import {motion} from "framer-motion";
 
 interface ModalProps {
     open: boolean
@@ -9,23 +10,35 @@ interface ModalProps {
     children: ReactElement
 }
 
+const spring = {
+    type: "spring",
+    damping: 10,
+    stiffness: 100
+}
+
 const PrimaryModal = ({open, children, onDismiss, className}: ModalProps) => {
     return (
         <Modal
             open={open}
-            className={className}
             disablePortal
             onClose={onDismiss}
+            className={`${styles.modalContainer} ${className}`}
+
         >
             <Box
-                className={styles.modal}
                 sx={{
                     width: {xs: '90%', sm: '60%', lg: '35%'},
-                    backgroundColor: 'background.paper'
+                    backgroundColor: 'background.paper',
                 }}
+                component={motion.div}
+                transition={spring}
+                initial={{scale: 0}}
+                animate={{scale: 1}}
+                className={styles.modal}
             >
                 {children}
             </Box>
+
         </Modal>
     )
 }

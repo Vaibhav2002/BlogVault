15cde2eb73cc425447aaf273acff8fa3028d98d4
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-05 12:44:20 +0530

    Create Comment Section designed

---
+++ frontend/src/components/comments/CreateCommentSection.tsx
@@ -0,0 +1,70 @@
+import React, {useContext} from 'react';
+import * as yup from 'yup'
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import FormTextField from "@/components/form/FormTextField";
+import {IconButton, Stack} from "@mui/material";
+import {RiSendPlaneFill} from "react-icons/ri";
+import {HttpError} from "@/data/HttpErrors";
+import {createComment} from "@/data/dataSources/CommentDataSource";
+import Comment from "@/data/models/Comment";
+import {Send, SendRounded} from "@mui/icons-material";
+import {AuthModalsContext} from "@/components/modals/auth/AuthModal";
+import useAuthenticatedUser from "@/hooks/useAuthenticatedUser";
+
+interface CreateCommentSectionProps {
+    blogId:string
+    className?: string,
+    onCommentCreated:(comment:Comment) => void
+}
+
+const createCommentSchema = yup.object({
+    comment: yup.string().max(300, "Comment cannot be longer than 300 characters")
+})
+
+type CreateCommentValues = yup.InferType<typeof createCommentSchema>
+
+const CreateCommentSection = ({blogId, onCommentCreated, className}: CreateCommentSectionProps) => {
+    const {showLogin} = useContext(AuthModalsContext)
+    const {user} = useAuthenticatedUser()
+
+    const {control, handleSubmit, reset} = useForm<CreateCommentValues>({
+        resolver: yupResolver(createCommentSchema)
+    })
+
+    const onSubmit = async({comment}:CreateCommentValues) => {
+        if(!comment) return
+        if(!user) return showLogin()
+        try{
+            const response = await createComment(blogId, {comment})
+            onCommentCreated(response)
+            reset()
+        } catch(e){
+            console.error(e)
+            if(e instanceof HttpError) alert(e.message)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack direction='row' spacing={1}>
+                <FormTextField
+                    fullWidth={false}
+                    control={control}
+                    name='comment'
+                    maxLength={300}
+                    showLength
+                    maxRows={4}
+                    placeholder='Enter your comment here'
+                    InputProps={{
+                        endAdornment: (
+                            <IconButton size='small' type='submit'><SendRounded/></IconButton>
+                        )
+                    }}
+                />
+            </Stack>
+        </form>
+    )
+}
+
+export default CreateCommentSection;
--- frontend/src/data/dataSources/CommentDataSource.ts
+++ frontend/src/data/dataSources/CommentDataSource.ts
@@ -1,9 +1,19 @@
 import api from "./AxiosInstance"
-import {CommentPage} from "@/data/models/Comment";
+import Comment, {CommentPage} from "@/data/models/Comment";
 
 export const getAllComments = async (blogId:string, lastCommentId?:string) => {
     const response = await api.get<CommentPage>(`/blogs/${blogId}/comments`, {
         params: {continueAfterId: lastCommentId}
     })
     return response.data as CommentPage
+}
+
+interface CommentValues{
+    comment:string,
+    parentCommentId?:string
+}
+
+export const createComment = async (blogId:string, comment:CommentValues) => {
+    const response = await api.post<Comment>(`/blogs/${blogId}/comment`, comment)
+    return response.data
 }

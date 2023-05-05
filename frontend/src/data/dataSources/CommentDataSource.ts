dd7956e31ebf7413d854e93bc4176654417a50c7
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-05 15:12:13 +0530

    Comment Modifying API calls added in DataSource

--- frontend/src/data/dataSources/CommentDataSource.ts
+++ frontend/src/data/dataSources/CommentDataSource.ts
@@ -8,12 +8,32 @@ export const getAllComments = async (blogId:string, lastCommentId?:string) => {
     return response.data as CommentPage
 }
 
-interface CommentValues{
-    comment:string,
-    parentCommentId?:string
+interface CommentValues {
+    comment: string,
+    parentCommentId?: string
 }
 
-export const createComment = async (blogId:string, comment:CommentValues) => {
+export const createComment = async (blogId: string, comment: CommentValues) => {
     const response = await api.post<Comment>(`/blogs/${blogId}/comment`, comment)
     return response.data
+}
+
+interface UpdateCommentValues {
+    comment: string,
+}
+
+export const updateComment = async (commentId: string, comment: UpdateCommentValues) => {
+    const response = await api.patch<Comment>(`blogs/comments/${commentId}`, comment)
+    return response.data
+}
+
+export const deleteComment = async (commentId: string) => {
+    await api.delete(`/blogs/comments/${commentId}`)
+}
+
+export const getCommentReplies = async (commentId: string, lastCommentId?: string) => {
+    const response = await api.get<CommentPage>(`/blogs/comments/${commentId}/replies`, {
+        params: {continueAfterId: lastCommentId}
+    })
+    return response.data as CommentPage
 }

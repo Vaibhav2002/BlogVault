e433acb43db848791e2228cee510a3208ddb2cf7
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-08 21:12:34 +0530

    Trending Topics in Discover Section added

--- frontend/src/components/chipGroup/ChipGroup.tsx
+++ frontend/src/components/chipGroup/ChipGroup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, BoxProps} from "@mui/material";
+import {Box, BoxProps, Skeleton} from "@mui/material";
 import styles from "@/components/chipGroup/ChipGroup.module.css";
 import StyledChip from "@/components/styled/Chip";
 
@@ -7,23 +7,44 @@ interface ChipGroupProps<T> {
     items: T[]
     getLabel: (chip: T) => string
     gap?: number
-
     size?: "small" | "medium"
     className?: string
 }
 
-const ChipGroup = <T extends unknown>({
-                                          items,
-                                          getLabel,
-                                          gap = 2,
-                                          size = "medium",
-                                          className,
-                                          ...props
-                                      }: ChipGroupProps<T> & BoxProps) => {
+const ChipGroup = <T extends unknown>(
+    {items, getLabel, gap = 2, size = "medium", className, ...props}: ChipGroupProps<T> & BoxProps
+) => {
     return (
         <Box className={`${styles.container} ${className}`} gap={gap} {...props}>
             {items.map((chip, index) =>
                 <StyledChip key={index} label={getLabel(chip)} size={size}/>
+            )}
+        </Box>
+    )
+}
+
+interface ChipGroupSkeletonProps {
+    count: number
+    gap?: number
+    size?: "small" | "medium"
+    className?: string
+}
+
+export const ChipGroupSkeleton = ({count, gap = 2, size = 'medium', className}: ChipGroupSkeletonProps) => {
+    const width = size === "small" ? 60 : 100
+    const randomizedWidth = () => width + ((Math.random() - 0.5) * width)
+    const height = 32
+
+    return (
+        <Box className={`${styles.container} ${className}`} gap={gap}>
+            {[...Array(count)].map((chip, index) =>
+                <Skeleton
+                    variant='rectangular'
+                    width={randomizedWidth()}
+                    height={height}
+                    key={index}
+                    sx={{borderRadius: 1}}
+                />
             )}
         </Box>
     )
--- frontend/src/data/dataSources/TopicDataSource.ts
+++ frontend/src/data/dataSources/TopicDataSource.ts
@@ -4,3 +4,8 @@ export const getAllTopics = async () => {
     const response = await api.get<Topic[]>(`/topics`)
     return response.data
 }
+
+export const getTrendingTopics = async () => {
+    const response = await api.get<Topic[]>(`/topics/trending`)
+    return response.data
+}

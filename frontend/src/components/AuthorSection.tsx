d612486914ed09faa70904c9b6f31bfb02643fec
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-08 21:18:24 +0530

    Navigation from AuthorSection added

--- frontend/src/components/AuthorSection.tsx
+++ frontend/src/components/AuthorSection.tsx
@@ -3,6 +3,8 @@ import User from "@/data/models/User";
 import {Stack, Typography} from "@mui/material";
 import UserAvatar from "@/components/Avatar";
 import Dot from "@/components/Dot";
+import {useRouter} from "next/router";
+import Route, {getUserRoute} from "@/utils/Routes";
 
 interface AuthorSectionProps {
     author: User,
@@ -11,17 +13,19 @@ interface AuthorSectionProps {
     className?: string
 }
 
-const AuthorSection = ({
-                           author: {profilePicUrl, username},
-                           avatarSize = 'medium',
-                           date,
-                           className
-                       }: AuthorSectionProps) => {
+const AuthorSection = (
+    {author: {profilePicUrl, username}, avatarSize = 'medium', date, className}: AuthorSectionProps
+) => {
+    const router = useRouter()
+    const onClick = (e: any) => {
+        e.stopPropagation()
+        router.push(username ? getUserRoute(username) : Route.Home)
+    }
 
     return (
-        <Stack alignItems="center" direction="row" spacing={1}>
-            <UserAvatar url={profilePicUrl} size={avatarSize}/>
-            <Typography variant="caption" color="text.secondary">{username}</Typography>
+        <Stack alignItems="center" direction="row" spacing={1} sx={{cursor: 'default'}}>
+            <UserAvatar url={profilePicUrl} size={avatarSize} onClick={onClick}/>
+            <Typography variant="caption" color="text.secondary" onClick={onClick}>{username}</Typography>
             <Dot/>
             <Typography variant="caption" color="text.secondary">{date}</Typography>
         </Stack>

2205ac28348cd3935519d2989d74544d294adfbb
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-14 13:27:56 +0530

    Added iSaved field in SavedBlogs

--- frontend/src/data/dataSources/SavedBlogDataSource.ts
+++ frontend/src/data/dataSources/SavedBlogDataSource.ts
@@ -2,8 +2,15 @@ import api from './AxiosInstance';
 import {BlogPage} from "@/data/models/Blog";
 
 export const getSavedBlogs = async (page: number = 1) => {
-    const response = await api.get<BlogPage>(`/blogs/saved?page?=${page}`)
-    return response.data
+    return await api.get<BlogPage>(`/blogs/saved?page?=${page}`)
+        .then(response => response.data)
+        .then(blogPage => ({
+            ...blogPage,
+            blogs: blogPage.blogs.map(blog => ({
+                ...blog,
+                isSaved: true
+            }))
+        }))
 }
 
 export const saveBlog = async (slug: string) => {

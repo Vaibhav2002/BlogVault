cb3e3af05ccf6f824a922739e6e690cef9930a42
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-14 12:45:14 +0530

    SavedBlog model uniqueness fixed

--- backend/src/models/entities/SavedBlog.ts
+++ backend/src/models/entities/SavedBlog.ts
@@ -1,8 +1,8 @@
 import {InferSchemaType, model, Schema} from "mongoose";
 
 const savedBlogSchema = new Schema({
-    blog: {type: Schema.Types.ObjectId, ref: 'Blog', unique: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User', unique: true},
+    blog: {type: Schema.Types.ObjectId, ref: 'Blog'},
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
 }, {timestamps: true});
 
 type SavedBlog = InferSchemaType<typeof savedBlogSchema>;

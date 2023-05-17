e22db50c1eb8c19b8bef9802c0e52ad31ab08040
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-17 21:19:39 +0530

    Navigation to User Profile from Discover Page added

--- frontend/src/components/author/AuthorProfileItem.tsx
+++ frontend/src/components/author/AuthorProfileItem.tsx
@@ -4,19 +4,25 @@ import {Box, BoxProps, Typography} from "@mui/material";
 import UserAvatar from "@/components/Avatar";
 import BottomGradientBox from "@/components/styled/BottomGradientBox";
 import MultilineText from "@/components/styled/MultilineText";
+import Routes, {getUserRoute} from "@/utils/Routes";
+import {useRouter} from "next/router";
 
 interface AuthorProfileItemProps {
     author: User
     className?: string
 }
 
-const AuthorProfileItem = ({
-                               author: {profilePicUrl, username, displayName},
-                               className,
-                               ...props
-                           }: AuthorProfileItemProps & BoxProps) => {
+const AuthorProfileItem = (
+    {author: {profilePicUrl, username, displayName}, className, ...props}: AuthorProfileItemProps & BoxProps
+) => {
+    const router = useRouter()
+    const onClick = async () => {
+        await router.push(username ? getUserRoute(username) : Routes.Home)
+    }
+
     return (
-        <Box sx={{position: 'relative', aspectRatio: '3/4', borderRadius: 2, overflow: 'hidden'}} {...props}>
+        <Box onClick={onClick}
+             sx={{position: 'relative', aspectRatio: '3/4', borderRadius: 2, overflow: 'hidden'}} {...props}>
             <UserAvatar size='100' variant='rounded' url={profilePicUrl}/>
             <BottomGradientBox color='white' padding={2}>
                 <Typography variant='caption' paddingBottom={0}>{`@${username}`}</Typography>

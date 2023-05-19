59b5c71b9e71da500a9cc2cb338989f5a5687acc
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-19 23:30:38 +0530

    NextConfig added for Deployed Backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["localhost", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+        domains: [
+            "localhost",
+            "lh3.googleusercontent.com",
+            "avatars.githubusercontent.com",
+            process.env.NEXT_PUBLIC_BACKEND_URL
+        ],
         deviceSizes: [600, 900, 1200, 1536]
     }
 }

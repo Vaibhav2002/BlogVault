b9cc17dbd555612f510c1908f7701cb9045e0c17
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-20 16:01:12 +0530

    NextJs Config updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
             "localhost",
             "lh3.googleusercontent.com",
             "avatars.githubusercontent.com",
-            process.env.NEXT_PUBLIC_BACKEND_URL
+            'api.blogvault.vaibhavjaiswal.tech'
         ],
         deviceSizes: [600, 900, 1200, 1536]
     }

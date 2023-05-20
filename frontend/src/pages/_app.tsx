361dbe7e49387f03cfb2dbbde5aaebfb6231eaca
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-05-20 21:51:02 +0530

    Social Media Preview fixed

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -30,7 +30,7 @@ export default function App({Component, pageProps, emotionCache = clientSideEmot
                 <meta name='viewport' content='width=device-width, initial-scale=1'/>
                 <link rel='icon' href='/favicon.ico'/>
                 <meta property="og:image" key='og:image'
-                      content="https://c792f92b82df4e.lhr.life/social_media_preview.png"/>
+                      content="https://blogvault.vaibhavjaiswal.tech/social_media_preview.png"/>
                 <meta name="twitter:card" content="summary_large_image"/>
             </Head>
             <CacheProvider value={emotionCache}>
